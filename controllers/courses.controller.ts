eb59356880c6b6b0f4be7d7029797ee4bcb91ec3
aishacodes <aishatabdulfatah81@gmail.com>
2024-10-11 14:56:15 +0200

    feat: edit and delete course

--- controllers/courses.controller.ts
+++ controllers/courses.controller.ts
@@ -22,9 +22,49 @@ export const getCourseById = async (req: Request, res: Response) => {
 };
 
 export const addCourse = async (req: Request, res: Response) => {
-  const course: ICourse = req.body;
+  const course = req.body;
+
   const courses = await getCousesFromFile();
-  courses.push(course);
+
+  const maxId =
+    courses.length > 0
+      ? Math.max(...courses.map((n: ICourse) => Number(n.id)))
+      : 0;
+
+  const newCourseId = maxId + 1;
+
+  courses.push({ id: newCourseId, ...course });
   await addCoursesToFile(courses);
-  res.status(201).json({ message: "Course created succcessfully", course });
+  res.status(201).json({
+    message: "Course created succcessfully",
+    course: { id: newCourseId, ...course },
+  });
+};
+
+export const deleteCourse = async (req: Request, res: Response) => {
+  const courseId = req.params.id;
+  let courses = await getCousesFromFile();
+  courses = courses.filter((course: ICourse) => course.id !== Number(courseId));
+  await addCoursesToFile(courses);
+
+  res.status(204).end();
+};
+
+export const updateCourse = async (req: Request, res: Response) => {
+  const courseId = req.params.id;
+  const courseToUpdate = req.body;
+
+  const courses = await getCousesFromFile();
+  const courseIndex = courses.findIndex(
+    (course: ICourse) => course.id === Number(courseId)
+  );
+
+  if (courseIndex !== -1) {
+    courses[courseIndex] = { ...courses[courseIndex], ...courseToUpdate };
+    await addCoursesToFile(courses);
+  }
+  res.status(201).json({
+    message: "Course updated succcessfully",
+    course: { ...courses[courseIndex] },
+  });
 };
--- routes/courses.routes.ts
+++ routes/courses.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   addCourse,
+  deleteCourse,
   getCourse,
   getCourseById,
+  updateCourse,
 } from "../controllers/courses.controller";
 
 const router = Router();
@@ -10,5 +12,7 @@ const router = Router();
 router.get("/courses", getCourse);
 router.get("/courses/:id", getCourseById);
 router.post("/courses", addCourse);
+router.delete("/courses/:id", deleteCourse);
+router.patch("/courses/:id", updateCourse);
 
 export default router;
